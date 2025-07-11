0c2be9b135a1e77dd2e216f1a628b546376302e6
Eduarda Nere Rodrigues <eduardanere890@gmail.com>
2025-07-11 19:56:27 -0300

    Delete Downloads/projeto directory

--- Downloads/projeto/src/services/api.ts
+++
@@ -1,9 +0,0 @@
-export const getRestaurants = async () => {
-    const response = await fetch('https://fake-api-tau.vercel.app/api/efood/restaurantes');
-    return await response.json();
-};
-
-export const getRestaurantById = async (id: string) => {
-    const response = await fetch(`https://fake-api-tau.vercel.app/api/efood/restaurantes/${id}`);
-    return await response.json();
-};
